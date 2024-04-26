49cbb71f855dce8f10015500d7222566b05d0294
Gourav Ghosal <slggourav@gmail.com>
2024-04-26 22:55:52 +0530

    added next/themes config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import localFont from "next/font/local";
+import { ThemeProvider } from "./theme-provider";
 
 const googleSans = localFont({
   src: [
@@ -27,7 +28,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${googleSans.className}`}>{children}</body>
+      <body className={`${googleSans.className}`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { ThemeToggle } from "@/components/theme-toggler";
+
 export default function Home() {
   return <>Coming soon</>;
 }
