69cdc9865a5ed4f5a3ce721d9fcbbab2ce7bef97
Gourav Ghosal <slggourav@gmail.com>
2024-05-08 23:32:01 +0530

    added blurred nav on scroll

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const debounce = (callback: any, delay: number) => {
+  let timeoutId: NodeJS.Timeout;
+  return (...args: any) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
